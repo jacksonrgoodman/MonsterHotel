05c3a9c4974097eaad33e8e079cb6e78baf865dc
Jackson <jacksonrgoodman@gmail.com>
2021-07-26 00:06:54 -0500

    app settings update, sql database cleanup, nuget install

--- SQL/01_Db_Create.sql
+++ SQL/01_Db_Create.sql
@@ -6,95 +6,96 @@ USE [MonsterHotel]
 GO
 
 
-DROP TABLE IF EXISTS [UserProfile]; --!
-DROP TABLE IF EXISTS [UserType]; --*
-DROP TABLE IF EXISTS [Ticket]; --*
-DROP TABLE IF EXISTS [TicketType]; --*
-DROP TABLE IF EXISTS [TicketStatus]; --*
-DROP TABLE IF EXISTS [Rooms]; --*
-DROP TABLE IF EXISTS [Stay]; --*
+DROP TABLE IF EXISTS [UserProfile];
+DROP TABLE IF EXISTS [UserType];
+DROP TABLE IF EXISTS [Ticket];
+DROP TABLE IF EXISTS [TicketType];
+DROP TABLE IF EXISTS [TicketStatus];
+DROP TABLE IF EXISTS [Rooms];
+DROP TABLE IF EXISTS [Stay];
 GO 
+
 CREATE TABLE [UserProfile] (
-  [Id] int PRIMARY KEY,
-  [FireBaseId] nvarchar(255),
-  [DisplayName] nvarchar(255),
-  [FirstName] nvarchar(255),
-  [LastName] nvarchar(255),
-  [Email] nvarchar(255),
+  [Id] int PRIMARY KEY NOT NULL IDENTITY,
+  [FireBaseId] NVARCHAR(28) NOT NULL,
+  [DisplayName] nvarchar(50) NOT NULL,
+  [FirstName] nvarchar(50) NOT NULL,
+  [LastName] nvarchar(50) NOT NULL,
+  [Email] nvarchar(555) NOT NULL,
   [ImageLocation] nvarchar(255),
-  [UserTypeId] int,
-  [DateCreated] datetime,
-  [IsCheckedIn] bool,
-  [IsActive] bool
+  [UserTypeId] integer NOT NULL,
+  [DateCreated] datetime NOT NULL,
+  [IsCheckedIn] BIT NOT NULL DEFAULT 0,
+  [IsActive] BIT NOT NULL DEFAULT 0,
 )
 GO
 
 CREATE TABLE [UserType] (
-  [Id] int PRIMARY KEY,
-  [Name] nvarchar(255)
+  [Id] int PRIMARY KEY NOT NULL IDENTITY,
+  [Name] nvarchar(50) NOT NULL
 )
 GO
 
 CREATE TABLE [Ticket] (
-  [Id] int PRIMARY KEY,
-  [TicketTypeId] int,
-  [Title] nvarchar(255),
-  [Description] VARCHAR,
+  [Id] int PRIMARY KEY NOT NULL IDENTITY,
+  [TicketTypeId] integer NOT NULL,
+  [Title] nvarchar(50) NOT NULL,
+  [Description] nvarchar(255) NOT NULL,
   [ImageUrl] nvarchar(255),
-  [CreateDateTime] datetime,
-  [UserProfileId] int,
-  [TicketStatusId] int,
-  [IsActive] bool
+  [CreateDateTime] datetime NOT NULL,
+  [UserProfileId] integer NOT NULL,
+  [TicketStatusId] integer NOT NULL,
+  [IsActive] BIT NOT NULL DEFAULT 0,
 )
 GO
 
 CREATE TABLE [TicketType] (
-  [Id] int PRIMARY KEY,
-  [Name] nvarchar(255)
+  [Id] int PRIMARY KEY NOT NULL IDENTITY,
+  [Name] nvarchar(50) NOT NULL
 )
 GO
 
 CREATE TABLE [TicketStatus] (
   [Id] INTEGER PRIMARY KEY NOT NULL IDENTITY(1, 1),
-  [Message] VARCHAR
+  [Name] nvarchar(50) NOT NULL
 )
 GO
 
 CREATE TABLE [Rooms] (
-  [Id] int PRIMARY KEY,
-  [Floor] int,
-  [Name] nvarchar(255)
+  [Id] int PRIMARY KEY NOT NULL IDENTITY,
+  [Floor] integer NOT NULL,
+  [Name] nvarchar(50) NOT NULL
 )
 GO
 
 CREATE TABLE [Stay] (
-  [Id] int PRIMARY KEY,
-  [GuestId] int,
-  [HandlerId] int,
-  [RoomId] int,
-  [CheckInTime] datetime,
-  [CheckOutTime] datetime,
-  [IsCheckedIn] bool,
-  [IsActive] bool
+  [Id] int PRIMARY KEY NOT NULL IDENTITY,
+  [GuestId] integer NOT NULL,
+  [HandlerId] integer NOT NULL,
+  [RoomId] integer NOT NULL,
+  [CheckInTime] datetime NOT NULL,
+  [CheckOutTime] datetime NOT NULL,
+  [IsCheckedIn] BIT NOT NULL DEFAULT 0,
+  [IsActive] BIT NOT NULL DEFAULT 0,
 )
 GO
 
-ALTER TABLE [Rooms] ADD FOREIGN KEY ([Id]) REFERENCES [Stay] ([RoomId])
+ALTER TABLE [Stay] ADD FOREIGN KEY ([RoomId]) REFERENCES [Rooms] ([Id])
 GO
 
-ALTER TABLE [UserProfile] ADD FOREIGN KEY ([Id]) REFERENCES [Stay] ([GuestId])
+ALTER TABLE [Stay] ADD FOREIGN KEY ([GuestId]) REFERENCES [UserProfile] ([Id])
 GO
 
-ALTER TABLE [UserProfile] ADD FOREIGN KEY ([Id]) REFERENCES [Stay] ([HandlerId])
+ALTER TABLE [Stay] ADD FOREIGN KEY ([HandlerId]) REFERENCES [UserProfile] ([Id])
 GO
 
 ALTER TABLE [UserProfile] ADD FOREIGN KEY ([UserTypeId]) REFERENCES [UserType] ([Id])
 GO
 
-ALTER TABLE [UserProfile] ADD FOREIGN KEY ([Id]) REFERENCES [Ticket] ([UserProfileId])
+ALTER TABLE [Ticket] ADD FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id])
 GO
 
-ALTER TABLE [TicketStatus] ADD FOREIGN KEY ([Id]) REFERENCES [Ticket] ([TicketStatusId])
+ALTER TABLE [Ticket] ADD FOREIGN KEY ([TicketStatusId]) REFERENCES [TicketStatus] ([Id])
 GO
 
 ALTER TABLE [Ticket] ADD FOREIGN KEY ([TicketTypeId]) REFERENCES [TicketType] ([Id])
--- SQL/02_Seed_Data.sql
+++ SQL/02_Seed_Data.sql
@@ -6,15 +6,15 @@ insert into [UserType] ([Id], [Name]) VALUES (1, 'Admin'), (2, 'Guest');
 set identity_insert [UserType] off
 
 set identity_insert [Ticket] on
-insert into [Ticket] ([Id], [TicketTypeId], [Description], [ImageUrl], [CreateDateTime], [UserProfileId], [TicketStatusId], [IsActive]) 
-VALUES ( 1, 1, 'I Need a blood bag!', '', '2020-04-12', 2, 1, 0),
- ( 2, 3, 'There is a leak in my sink!', '', '2020-04-12', 3, 1, 0),
- ( 3, 4, 'Where is the tunes?', '', '2020-04-12', 3, 1, 0)
+insert into [Ticket] ([Id], [TicketTypeId],[Title], [Description], [ImageUrl], [CreateDateTime], [UserProfileId], [TicketStatusId], [IsActive]) 
+VALUES ( 1, 1, 'Need Help','I Need a blood bag!', '', '2020-04-12', 2, 1, 0),
+ ( 2, 3, 'Sink','There is a leak in my sink!', '', '2020-04-12', 3, 1, 0),
+ ( 3, 4, 'Atmosphere Issue','Where is the tunes?', '', '2020-04-12', 3, 1, 0)
  ;
 set identity_insert [Ticket] off
 
 set identity_insert [TicketType] on
-insert into [TicketType] ([Id], [Name]) VALUES (1, 'Food'), (2, 'Bill'), (3, 'Maintenance'), (4, 'Concierge'), (5, 'Parking'), (5, 'Security');
+insert into [TicketType] ([Id], [Name]) VALUES (1, 'Food'), (2, 'Bill'), (3, 'Maintenance'), (4, 'Concierge'), (5, 'Parking'), (6, 'Security');
 set identity_insert [TicketType] off
 
 set identity_insert [TicketStatus] on
@@ -40,9 +40,9 @@ VALUES ( 1, 2, 1, 11, '2020-08-06', '', 1, 0),
 set identity_insert [Stay] off
 
 set identity_insert [UserProfile] on
-insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FirebaseUserId, IsCheckedIn, IsActive) values (1, 'Foo', 'Foo', 'Barington', 'foo@bar.com', '2020-04-23', 'https://robohash.org/numquamutut.png?size=150x150&set=set2', 1, 'DjwjqnO5URZEe6iKqo0Tf88LeKt2', 0, 0);
-insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FirebaseUserId, IsCheckedIn, IsActive) values (2, 'vlad_impaler', 'Vlad', 'Sandwith', 'rvlad_impaler48@vampire.com.brx', '2020-04-23', 'https://robohash.org/numquamutut.png?size=150x150&set=set2', 2, 'GqaSp4chZzZr2Tk0JBLPCQAFtmI2', 1, 0);
-insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FirebaseUserId, IsCheckedIn, IsActive) values (3, 'Post-Modern', 'Frank', 'Stein', 'prometheus@timesonline.co.ukx', '2020-04-20', 'https://robohash.org/nisiautemet.png?size=150x150&set=set2', 2, 'GRpxCESoc0hRywhenmk71h1EzYu1', 1, 0);
-insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FirebaseUserId, IsCheckedIn, IsActive) values (4, 'cairoCruise', 'Cleo', 'Patra', 'cairocruise2@ow.lyx', '2020-01-13', 'https://robohash.org/molestiaemagnamet.png?size=150x150&set=set2', 2, 'EwExMJOhSxW2LHKyz4PMMTOcE3R2', 1, 0);
-insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FirebaseUserId, IsCheckedIn, IsActive) values (5, '_webby', 'Webby', 'Spyder', 'web@monster.hotel.brx', '2020-04-23', 'https://robohash.org/numquamutut.png?size=150x150&set=set2', 1, 'DjwjqnO5URZEe6iKqo0Tf88LeKt2', 0, 0);
+insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FireBaseId, IsCheckedIn, IsActive) values (1, 'Foo', 'Foo', 'Barington', 'foo@bar.com', '2020-04-23', 'https://robohash.org/numquamutut.png?size=150x150&set=set2', 1, '3qmM15UvKRW1p7EoEpphpp8dRLs1', 0, 0);
+insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FireBaseId, IsCheckedIn, IsActive) values (2, 'vlad_impaler', 'Vlad', 'Impaler', 'rvlad_impaler48@vampire.com.brx', '2020-04-23', 'https://robohash.org/numquamutut.png?size=150x150&set=set2', 2, 'qp2yqgdlVJNJPzMoomHRZpUpXnV2', 1, 0);
+insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FireBaseId, IsCheckedIn, IsActive) values (3, 'Post-Modern', 'Frank', 'Stein', 'prometheus@timesonline.co.ukx', '2020-04-20', 'https://robohash.org/nisiautemet.png?size=150x150&set=set2', 2, 'wzpGclgNgaUjmBuZdnhoYpYt5yb2', 1, 0);
+insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FireBaseId, IsCheckedIn, IsActive) values (4, 'cairoCruise', 'Cleo', 'Patra', 'cairocruise2@ow.lyx', '2020-01-13', 'https://robohash.org/molestiaemagnamet.png?size=150x150&set=set2', 2, 'SnJxLe4IFjQVxpn3q4llLGOnk572', 1, 0);
+insert into UserProfile (Id, DisplayName, FirstName, LastName, Email, DateCreated, ImageLocation, UserTypeId, FireBaseId, IsCheckedIn, IsActive) values (5, '_webby', 'Webby', 'Spyder', 'web@monster.hotel.brx', '2020-04-23', 'https://robohash.org/numquamutut.png?size=150x150&set=set2', 1, 'NklbCZTB0vXGeL1hIZFVKZsurHj2', 0, 0);
 set identity_insert [UserProfile] off
