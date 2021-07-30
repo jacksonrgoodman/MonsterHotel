ea7e84fb3063c1a18dfb66b2f3b4d93927a28d59
Jackson <jacksonrgoodman@gmail.com>
2021-07-30 00:58:18 -0500

    bugfixes

---
+++ MonsterHotel/client/src/components/UserProfile/GenericProfileCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+
+export default function UserProfileCard({ UserProfile }) {
+    console.log("UserProfile", UserProfile)
+    return (
+        <Card className="m-4">
+            <img src={UserProfile.imageLocation} />
+            <CardBody>
+                <p><strong>{UserProfile.userType.name} User: </strong>{UserProfile.fullName}</p>
+                <p><strong>Display Name: </strong>{UserProfile.displayName}</p>
+            </CardBody>
+        </Card>
+    );
+}
